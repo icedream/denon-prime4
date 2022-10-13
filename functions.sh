9d606b9337b6b79c291b05407cf9046cdad7f5ec
Carl Kittelberger <icedream@icedream.pw>
2022-10-13 10:25:57 +0200

    Add helper tool to update devices.txt.

--- functions.sh
+++ functions.sh
@@ -27,11 +27,11 @@ proc_args=()
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
-  -d|--device)
+  -d | --device)
     device="$2"
     shift 2
     ;;
-  -v|--vendor)
+  -v | --vendor)
     vendor="$2"
     shift 2
     ;;
@@ -51,19 +51,21 @@ log_debug "Set arguments to:" "${proc_args[@]}"
 export ENGINEOS_DEVICE="$device" ENGINEOS_VENDOR="$vendor"
 
 device_id=
-prime4_update_download_url=
-prime4_update_download_filename=
-prime4_updater_win_download_url=
-prime4_updater_win_download_filename=
+device_update_download_url=
+device_update_download_filename=
+device_application_name=
+device_updater_win_download_url=
+device_updater_win_download_filename=
 
-while read -r current_vendor current_device current_device_id current_img_download_url current_updater_download_url _; do
+while read -r current_vendor current_device current_device_id current_device_application_name current_img_download_url current_updater_download_url _; do
   if [ "$current_vendor" = "$vendor" ] && [ "$current_device" = "$device" ]; then
     device_id="$current_device_id"
-    prime4_update_download_url="$current_img_download_url"
-    prime4_updater_win_download_url="${current_img_download_url%.img}.exe"
+    device_update_download_url="$current_img_download_url"
+    device_updater_win_download_url="${current_img_download_url%.img}.exe"
     if [ -n "$current_updater_download_url" ]; then
-      prime4_updater_win_download_url="${current_updater_download_url}"
+      device_updater_win_download_url="${current_updater_download_url}"
     fi
+    device_application_name="$current_device_application_name"
     break
   fi
 done <devices.txt
@@ -73,8 +75,8 @@ if [ -z "$device_id" ]; then
   exit 1
 fi
 
-prime4_update_download_filename="${prime4_update_download_url##*/}"
-prime4_updater_win_download_filename="${prime4_updater_win_download_url##*/}"
-prime4_updater_win_download_filename="${prime4_updater_win_download_filename//+/ }"
+device_update_download_filename="${device_update_download_url##*/}"
+device_updater_win_download_filename="${device_updater_win_download_url##*/}"
+device_updater_win_download_filename="${device_updater_win_download_filename//+/ }"
 
 unpacked_img_dir="unpacked-img/$device_id"
