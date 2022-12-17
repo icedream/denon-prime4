5b654078e3fa7121045c9fb35a67b9a22c8e3ac8
Carl Kittelberger <icedream@icedream.pw>
2022-12-17 20:25:13 +0100

    Allow running other config targets through configure-buildroot.sh.

--- configure-buildroot.sh
+++ configure-buildroot.sh
@@ -3,11 +3,14 @@
 #./clone-buildroot.sh
 cp -v buildroot-config/.config buildroot/*/
 
-config_target=nconfig
-if [ -n "$DISPLAY" ]
-then
-  config_target=xconfig
+config_target="${1:-}"
+if [ -z "$config_target" ]; then
+  if [ -n "$DISPLAY" ]
+  then
+    config_target=xconfig
+  else
+    config_target=nconfig
+  fi
 fi
-
 make -C buildroot/*/ -j$(nproc) BR2_EXTERNAL=../../buildroot-customizations "$config_target"
 cp -v buildroot/*/.config buildroot-config
