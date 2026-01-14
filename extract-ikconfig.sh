770d1e4ee08dab7be2be545b759ab38b47406960
Carl Kittelberger <icedream@icedream.pw>
2026-01-14 05:05:54 +0100

    Build linux-headers to make script for extracting kernel config work.

--- extract-ikconfig.sh
+++ extract-ikconfig.sh
@@ -6,5 +6,7 @@ buildroot_path="buildroot/$(get_buildroot_version)"
 
 # TODO - detect linux-headers version
 
+make -C "${buildroot_path}" linux-headers
+
 do_mount cat /boot/zImage >zImage
 "${buildroot_path}"/output/build/linux-headers-*/scripts/extract-ikconfig zImage >"buildroot-customizations/board/inmusic/common/linux.config"
